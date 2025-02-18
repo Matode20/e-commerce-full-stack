45ced296837246568383b702c723c2db7af82be1
tobi martin <tobimartin@gmail.com>
2025-02-18 09:15:52 +0100

    feat: implement draft mode functionality, enhance order processing with Stripe webhook, and add currency formatting utility

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,10 +6,11 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true,
   stega: {
-    studioUrl: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/studio`
-      : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
+    studioUrl:
+      process.env.NODE_ENV === "production"
+        ? `https://${process.env.VERCEL_URL}/studio`
+        : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
   },
 });
